5bc9a5714c57c48a45f5d3cc5347b499414a70c0
Charts <muhammadshaharyar97@gmail.com>
2021-12-12 19:00:54 +0500

    Eyes To Ears Voice Assist

---
+++ app/src/main/java/com/example/eteva/TimePickerFragment.java
@@ -0,0 +1,24 @@
+package com.example.eteva;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.*;
+import androidx.fragment.app.DialogFragment;
+import android.text.format.DateFormat;
+
+
+import java.util.Calendar;
+
+
+public class TimePickerFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
+}
